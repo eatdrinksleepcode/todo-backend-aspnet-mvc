bac1493c281a885c6073c9da00db9e55e9bca648
David Nelson <eatdrinksleepcode@gmail.com>
2014-08-15 18:38:22 -0400

    Basic MVC on Mono

---
+++ TodoBackend/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace TodoBackend
+{
+	using System;
+	using System.Collections;
+	using System.ComponentModel;
+	using System.Web;
+	using System.Web.SessionState;
+
+	
+	public class Global : System.Web.HttpApplication
+	{
+		
+		protected void Application_Start(Object sender, EventArgs e)
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		void RegisterRoutes(RouteCollection routes)
+		{
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+		}
+	}
+}
+
